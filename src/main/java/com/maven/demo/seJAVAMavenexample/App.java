1d65103faebecbb6438d96f820f348b00c970db0
unknown <smynampati04@gmail.com>
2024-11-14 12:20:34 +0530

    PUSHING INTO GITHUB

---
+++ src/main/java/com/maven/demo/seJAVAMavenexample/App.java
@@ -0,0 +1,14 @@
+package com.maven.demo.seJAVAMavenexample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello SreejaM" );
+        System.out.println( "22BD1A053P" );
+    }
+}
